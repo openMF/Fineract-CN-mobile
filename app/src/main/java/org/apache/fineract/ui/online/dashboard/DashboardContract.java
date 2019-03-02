af057634620e084e2b6561b5ef9158b07f819883
Riya Singh <43132209+riyasingh1004@users.noreply.github.com>
2019-03-02 16:02:50 +0530

    feat:Double back press to exit the app.
    Updating dashboard to allow the user to double back press to exit the app.

--- app/src/main/java/org/apache/fineract/ui/online/dashboard/DashboardContract.java
+++ app/src/main/java/org/apache/fineract/ui/online/dashboard/DashboardContract.java
@@ -1,9 +1,35 @@
 package org.apache.fineract.ui.online.dashboard;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
 /**
  * @author Rajan Maurya
  *         On 27/07/17.
  */
 
-public class DashboardContract {
-}
+public class DashboardContract extends AppCompactActivity {
+   private static long BackPressed;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+        @Override
+         public void onBackPressed(){
+            if(BackPressed+2000>System.currentTimeMillis())
+            {
+                super.onBackPressed();
+            }
+            else{
+                Toast.makeText(getBaseContext(),"Press once again to exit",Toast.LENGTH_SHORT).show();
+            }
+           BackPressed = System.currentTimeMillis();
+        }
+    }
+
+
+
+
