91e98102e35db6e567bdd9b67ab1d083649b068f
Riya Singh <43132209+riyasingh1004@users.noreply.github.com>
2019-03-02 16:28:08 +0530

    Update DashboardContract.java

--- app/src/main/java/org/apache/fineract/ui/online/dashboard/DashboardContract.java
+++ app/src/main/java/org/apache/fineract/ui/online/dashboard/DashboardContract.java
@@ -1,8 +1,10 @@
 package org.apache.fineract.ui.online.dashboard;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
 import android.widget.Toast;
 
 /**
@@ -11,25 +13,30 @@ import android.widget.Toast;
  */
 
 public class DashboardContract extends AppCompactActivity {
-   private static long BackPressed;
+   private boolean doubleBackToExitPressedOnce = false;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
     }
         @Override
-         public void onBackPressed(){
-            if(BackPressed+2000>System.currentTimeMillis())
-            {
-                super.onBackPressed();
-            }
-            else{
-                Toast.makeText(getBaseContext(),"Press once again to exit",Toast.LENGTH_SHORT).show();
-            }
-           BackPressed = System.currentTimeMillis();
+    public void onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed();
+            return;
         }
-    }
+        this.doubleBackToExitPressedOnce = true;
+        Toast.makeText(this, "Press again to exit", Toast.LENGTH_SHORT)
+                .show();
+        new Handler().postDelayed(new Runnable() {
 
+            @Override
+            public void run() {
+                doubleBackToExitPressedOnce = false;
+            }
+        }, 2000);
+    }
+}
 
 
 
